0ab06ab1cc1c8a81c7d02bed5bf71edc1e8c6d44
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-23 00:00:47 -0700

    Inheritance

---
+++ src/oop/inheritance.kt
@@ -0,0 +1,24 @@
+package oop
+
+//Minimal example
+open class Base // in Kotlin classes are final by default, we need to declare class as an open class to inherit it
+
+class Child : Base()
+
+open class Shape(val name: String) {
+
+    open fun area() = 0.0
+}
+
+class Circle(name: String, val radius: Double) : Shape(name) {
+
+    override fun area() = Math.PI * Math.pow(radius, 2.0)
+}
+
+fun main() {
+    val smallCircle = Circle("Small circle", 2.0)
+
+    println(smallCircle.name)
+    println(smallCircle.radius)
+    println(smallCircle.area())
+}
