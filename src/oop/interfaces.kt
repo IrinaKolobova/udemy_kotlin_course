134cfcb7baf2adfaf85dbc50826b5e8db856c802
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-23 21:42:10 -0700

    Interfaces

---
+++ src/oop/interfaces.kt
@@ -0,0 +1,29 @@
+package oop
+
+interface Drivable {
+    val a: Int
+    fun drive()
+}
+
+class Bicycle : Drivable {
+    override val a: Int
+        get() = 42
+    override fun drive() {
+        println("Driving bicycle")
+    }
+
+    fun b() = 1
+}
+
+class Boat : Drivable {
+    override val a = 41
+    override fun drive() {
+        println("Driving boat")
+    }
+}
+
+fun main() {
+    val drivable: Drivable = Bicycle()
+    drivable.drive()
+    
+}
