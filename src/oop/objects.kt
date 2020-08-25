62b25018146fe3df6f46ae601657b331cf7a085e
Irina Kolobova <kolobovairinav@gmail.com>
2020-08-24 23:10:00 -0700

    Object Declarations as Singletons

---
+++ src/oop/objects.kt
@@ -0,0 +1,21 @@
+package oop
+
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+
+object CountryFactory {
+
+    val a = 4
+    fun createCountry() = Country("Australia")
+}
+
+object  DefaultListener : MouseAdapter() {
+    override fun mouseClicked(e: MouseEvent?) {
+        println("Mouse was clicked")
+    }
+}
+
+fun main() {
+    CountryFactory.a
+    CountryFactory.createCountry()
+}
